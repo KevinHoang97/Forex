36513f76f4a8b8b347a7b4bf85dad5b72ffe2f6b
Kevin Hoang <x00137044@mytudublin.ie>
2021-08-25 22:30:00 +0100

    Move ServiceExtension location

--- Service/ServiceExtention.cs
+++ Service/ServiceExtention.cs
@@ -7,13 +7,13 @@ using System.Threading.Tasks;
 
 namespace Forex
 {
-    public static class ServiceExtensions
+    public static class ServiceExtension
     {
 
         public static async Task<T> GetJsonAsync<T>(this HttpClient httpClient, string url)
         {
             var request = new HttpRequestMessage(HttpMethod.Get, url); //makes request
-            request.Headers.Add("Authorization", "Bearer dc4ee50cf216bdffdec436646c7d321b-c6cd308844f735f22196af9f27ff8720"); //no point in hiding this, in public header anyway.
+            request.Headers.Add("Authorization", "Bearer 50f5b6cf1d7ec2e9adbb46f534883f5a-d9f458ef2e65108b1ba1ade2f0f68926"); //no point in hiding this, in public header anyway.
 
             var response = await httpClient.SendAsync(request);
             response.Headers.Add("Access-Control-Allow-Origin", "*"); //Allows for CORS in response headers
